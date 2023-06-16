bff011269526e8200081cdf815a3e014b8de6566
Marianna Mazzette <20104120@mail.wit.ie>
2023-06-16 21:26:49 +0100

    first commit

---
+++ routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { dashboardController } from "./controllers/dashboard-controller.js";
+import { playlistController } from "./controllers/playlist-controller.js";
+import { aboutController } from "./controllers/about-controller.js";
+
+export const router = express.Router();
+
+router.get("/", dashboardController.index);
+router.get("/dashboard", dashboardController.index);
+router.post("/dashboard/addplaylist", dashboardController.addPlaylist);
+router.get("/dashboard/deleteplaylist/:id", dashboardController.deletePlaylist);
+router.get("/playlist/:id", playlistController.index);
+router.post("/playlist/:id/addtrack", playlistController.addTrack);
+router.get("/playlist/:playlistid/deletetrack/:trackid", playlistController.deleteTrack);
+router.get("/about", aboutController.index);
